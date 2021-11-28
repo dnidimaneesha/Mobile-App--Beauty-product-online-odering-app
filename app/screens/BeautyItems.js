10285d2cad990a9077cb2f5ccb4fd4fddc721b1a
dnidimaneesha <96563@fhss.sjp.ac.lk>
2021-11-28 10:45:35 +0530

    create BeautyItems screen and navigate it

---
+++ app/screens/BeautyItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const BeautyItems = ({navigation}) => {
+  return (
+    <View>
+      <Text>Beauty Items</Text>
+    </View>
+  );
+};
+
+export default BeautyItems;
