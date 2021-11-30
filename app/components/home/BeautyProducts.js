243e88e37f4009dddf5a841188419bf6e3929d4d
dnidimaneesha <96563@fhss.sjp.ac.lk>
2021-11-30 11:12:34 +0530

    create beautyProducts component and const LocalBeautyProduct

---
+++ app/components/home/BeautyProducts.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const localBeautyProducts = [
+  {
+    name: 'Big City Lover Matara',
+    image_url:
+      'https://quintessenceblog.com/wp-content/uploads/2016/07/Beautycounter-on-Nantucket.jpg',
+    categories: ['beauty shop', 'Lover'],
+    price: 'RS',
+    reviews: 1244,
+    rating: 4.9,
+  },
+  {
+    name: 'Big City Lover Akurassa',
+    image_url:
+      'https://www.weekendnotes.com/im/003/09/yuki-house-japanese-korean-makeup-bourke-st1.jpg',
+    categories: ['beauty shop', 'Lover'],
+    price: 'RS',
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: 'Big City Lover Deniyaya',
+    image_url:
+      'https://cdn-design.amorepacific.com/contents/2020/08/13222145/Landing_Content1-1.jpg',
+    categories: ['beauty shop', 'Lover'],
+    price: 'RS',
+    reviews: 1244,
+    rating: 3.7,
+  },
+];
