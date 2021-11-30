b690987ac51c3188504818db4af6b8c1b4c3a229
dnidimaneesha <96563@fhss.sjp.ac.lk>
2021-11-30 17:43:55 +0530

    create beautyProduct function in beautyProduct component

--- app/components/home/BeautyProducts.js
+++ app/components/home/BeautyProducts.js
@@ -31,3 +31,63 @@ export const localBeautyProducts = [
     rating: 3.7,
   },
 ];
+
+export default function BeautyProducts({navigation, ...props}) {
+  return (
+    <>
+      {props.shopData.map((shop, index) => (
+        <TouchableOpacity
+          key={index}
+          activeOpacity={0.7}
+          style={{marginTop: 15}}>
+          }>
+          <View
+            key={index}
+            style={{marginTop: 10, padding: 15, backgroundColor: 'white'}}>
+            <BeautyImage image={shop.image_url} />
+            <BeautyInfo name={shop.name} rating={shop.rating} />
+          </View>
+        </TouchableOpacity>
+      ))}
+    </>
+  );
+}
+
+const BeautyImage = props => (
+  <>
+    <Image
+      source={{
+        uri: props.image,
+      }}
+      style={{width: '100%', height: 180}}
+    />
+    <TouchableOpacity style={{position: 'absolute', right: 20, top: 20}}>
+      <Icon name="heart-outline" color="white" size={30} />
+    </TouchableOpacity>
+  </>
+);
+
+const BeautyInfo = props => (
+  <View
+    style={{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: 10,
+    }}>
+    <View>
+      <Text style={{fontSize: 15, fontWeight: 'bold'}}>{props.name}</Text>
+      <Text style={{fontSize: 13, color: 'gray'}}>every beauty Products</Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: '#faf1f8',
+        height: 25,
+        width: 40,
+        alignItems: 'center',
+        borderRadius: 15,
+      }}>
+      <Text> {props.rating} </Text>
+    </View>
+  </View>
+);
