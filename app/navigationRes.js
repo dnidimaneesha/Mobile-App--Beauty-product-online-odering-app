7d77d4b026881552d924e05926500814ea585bf9
dnidimaneesha <96563@fhss.sjp.ac.lk>
2021-12-02 16:39:09 +0530

    wrap app using reducx in navigation.js file

--- app/navigationRes.js
+++ app/navigationRes.js
@@ -5,6 +5,10 @@ import Home from './screens/Home';
 import LoadingScreen from './screens/LoadingScreen';
 import BeautyItems from './screens/BeautyItems';
 import ShopDetails from './screens/ShopDetails';
+import {Provider as ReduxProvider} from 'react-redux';
+import configureStore from './redux/store';
+
+const store = configureStore();
 
 export default function RootNavigation() {
   const Stack = createStackNavigator();
@@ -14,15 +18,17 @@ export default function RootNavigation() {
   };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="LoadignScreen"
-        screenOptions={screenOptions}>
-        <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="BeautyItems" component={BeautyItems} />
-        <Stack.Screen name="ShopDetails" component={ShopDetails} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="LoadignScreen"
+          screenOptions={screenOptions}>
+          <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="BeautyItems" component={BeautyItems} />
+          <Stack.Screen name="ShopDetails" component={ShopDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
   );
 }
