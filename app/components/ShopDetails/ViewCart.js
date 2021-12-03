19f5870fed2df28e09170b207c6eacf03888f8e0
dnidimaneesha <96563@fhss.sjp.ac.lk>
2021-12-03 19:17:27 +0530

    modified orderItem screen interface

--- app/components/ShopDetails/ViewCart.js
+++ app/components/ShopDetails/ViewCart.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Modal, StyleSheet} from 'react-native';
 import {useSelector} from 'react-redux';
+import OrderItem from './OderItems';
 
 export default function ViewCart() {
   const [modalVisible, setModalVisible] = useState(false);
@@ -15,8 +16,102 @@ export default function ViewCart() {
     currency: 'USD',
   });
 
+  const styles = StyleSheet.create({
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      backgroundColor: 'rgba(0,0,0,0.7)',
+    },
+
+    modalCheckoutContainer: {
+      backgroundColor: 'white',
+      padding: 16,
+      height: 500,
+      borderWidth: 1,
+    },
+    delivery: {
+      textAlign: 'center',
+      fontWeight: '600',
+      fontSize: 18,
+      marginBottom: 10,
+      color: 'black',
+    },
+
+    subtotalContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 15,
+    },
+
+    subtotalText: {
+      textAlign: 'left',
+      fontWeight: '600',
+      fontSize: 15,
+      marginBottom: 10,
+    },
+  });
+
+  const checkoutModalContent = () => {
+    return (
+      <>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalCheckoutContainer}>
+            <Text style={styles.delivery}> Selected Items</Text>
+            {items.map((item, index) => (
+              <OrderItem key={index} item={item} />
+            ))}
+            <View style={styles.subtotalContainer}>
+              <Text style={styles.subtotalText}>Subtotal</Text>
+              <Text style={{marginRight: 20, color: 'black'}}>
+                RS:{totalUSD}.00
+              </Text>
+            </View>
+
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <TouchableOpacity
+                style={{
+                  marginTop: 20,
+                  backgroundColor: 'black',
+                  alignItems: 'center',
+                  padding: 13,
+                  borderRadius: 30,
+                  width: 300,
+                  position: 'relative',
+                }}
+                onPress={() => {
+                  addOrderToFireBase();
+                  setModalVisible(false);
+                }}>
+                <Text style={{color: 'white', fontSize: 20}}>Checkout</Text>
+                <Text
+                  style={{
+                    position: 'absolute',
+                    right: 20,
+                    color: 'white',
+                    fontSize: 15,
+                    top: 17,
+                  }}>
+                  Rs:
+                  {total ? totalUSD : ''}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </>
+    );
+  };
+
   return (
     <>
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        transparent={true}
+        onRequestClose={() => setModalVisible(false)}>
+        {checkoutModalContent()}
+      </Modal>
+
       {total ? (
         <View
           style={{
@@ -41,7 +136,8 @@ export default function ViewCart() {
                 position: 'relative',
                 justifyContent: 'flex-end',
                 flexDirection: 'row',
-              }}>
+              }}
+              onPress={() => setModalVisible(true)}>
               <Text style={{color: 'white', fontSize: 20, marginRight: 80}}>
                 Checkout
               </Text>
