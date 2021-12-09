bf6541ad94a98ddd54a728dcdf941cf1fbeb3216
dnidimaneesha <96563@fhss.sjp.ac.lk>
2021-12-09 17:15:45 +0530

    modified beautyItems.js screen and catItem component

--- app/screens/BeautyItems.js
+++ app/screens/BeautyItems.js
@@ -1,12 +1,189 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import CatItems from '../components/ItemCatagory/CatItems';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
-const BeautyItems = ({navigation}) => {
+
+const items = [
+  {
+    image: require('../assets/images/lips.png'),
+    text: 'LIPSTICK',
+  },
+
+  {
+    image: require('../assets/images/nail-Polish.png'),
+    text: 'NAIL POLISH',
+  },
+  {
+    image: require('../assets/images/makeup.png'),
+    text: 'MAKEUP ITEMS',
+  },
+  {
+    image: require('../assets/images/earrings.png'),
+    text: 'EARRINGS',
+  },
+  {
+    image: require('../assets/images/Wallet.png'),
+    text: 'WALLETS',
+  },
+  {
+    image: require('../assets/images/pin.png'),
+    text: 'HAIR PINS',
+  },
+];
+
+
+const Card = ({cat}) => {
+
   return (
-    <View>
-      <Text>Beauty product</Text>
+    <View style={style.cardContainer}>
+      <View style={style.cardImageContainer}>
+        <Image
+          source={cat.image}
+          style={{
+            width: '100%',
+            height: '100%',
+            resizeMode: 'contain',
+          }}
+        />
+      </View>
+      <View style={style.cardDetailsContainer}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text style={{fontWeight: 'bold', color: 'black', fontSize: 18}}>
+            {cat?.title}
+          </Text>
+        </View>
+        <View>
+          <Text style={{fontSize: 13, marginTop: 4}}>{cat?.type}</Text>
+          <Text style={{fontSize: 13, marginTop: 4}}>{cat?.price}</Text>
+        </View>
+      </View>
+      <View>
+        <BouncyCheckbox
+          iconStyle={{
+            borderColor: '#b289b0',
+            borderRadius: 0,
+            marginLeft: 20,
+          }}
+          fillColor="#c572c4"
+        />
+      </View>
     </View>
   );
 };
+
+
+
+
+const BeautyItems = ({navigation}) => {
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = React.useState(0);
+
+  const [fliteredCat, setfliteredCat] = React.useState([]);
+
+  const fliterCat = index => {
+    const currentCat = CatItems.filter(
+      item => item?.cat?.toUpperCase() == items[index].text,
+    )[0]?.CatItems;
+    setfliteredCat(currentCat);
+  };
+  React.useEffect(() => {
+    fliterCat(0);
+  }, []);
+
+  const ListCategories = () => {
+    return (
+      <View
+        style={{
+          marginTop: 15,
+          paddingVertical: 10,
+          paddingLeft: 20,
+        }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {items.map((item, index) => (
+            <View
+              key={'cat' + index}
+              style={{alignItems: 'center', marginRight: 30}}>
+              <TouchableOpacity
+                onPress={() => {
+                  setSelectedCategoryIndex(index);
+                  fliterCat(index);
+                }}
+                style={[
+                  style.categoryBtn,
+                  {
+                    backgroundColor:
+                      selectedCategoryIndex == index ? '#e5bfe4' : '#ffff',
+                  },
+                ]}>
+                <Image
+                  source={item.image}
+                  style={{
+                    width: 50,
+                    height: 40,
+                    resizeMode: 'contain',
+                  }}
+                />
+              </TouchableOpacity>
+              <Text style={{fontSize: 13, fontWeight: '900'}}>{item.text}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    );
+  };
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View>
+        <ListCategories />
+      </View>
+      <FlatList
+        data={fliteredCat}
+        renderItem={({item}) => <Card cat={item} navigation={navigation} />}
+      />
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  categoryBtn: {
+    height: 45,
+    width: 100,
+    marginRight: 5,
+    borderRadius: 30,
+    alignItems: 'center',
+    paddingHorizontal: 5,
+    flexDirection: 'row',
+  },
+  cardContainer: {
+    marginTop: 3,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  cardDetailsContainer: {
+    height: 120,
+    backgroundColor: '#ffff',
+    flex: 1,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  cardImageContainer: {
+    height: 150,
+    width: 130,
+    backgroundColor: '#eee',
+    borderRadius: 20,
+  },
+});
 
 export default BeautyItems;
